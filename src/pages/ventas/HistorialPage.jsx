f7650b159d91ad9266dd074490917c2eb2c58c0d
Pizzeria Admin <admin@lapizzeria.cl>
2026-04-22 13:43:19 -0400

    Fix: Importar useEffect faltante en el modulo de ventas

--- src/pages/ventas/HistorialPage.jsx
+++ src/pages/ventas/HistorialPage.jsx
@@ -1,4 +1,4 @@
-import { useState, useMemo } from 'react'
+import { useState, useMemo, useEffect } from 'react'
 import { useOrders } from '../../context/OrdersContext'
 import SalesFilterDrawer, {
   INIT_SALES_FILTERS,
